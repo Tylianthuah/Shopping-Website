86500b0e288a17d901b715323aa6d5d1e219d6b8
Tylianthuah <tylian15@gmail.com>
2022-06-21 08:38:46 +0530

    Added Localstorage

---
+++ data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "a",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "img/img-1.jpg",
+    },
+    {
+      id: "b",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "img/img-2.jpg",
+    },
+    {
+      id: "c",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "img/img-3.jpg",
+    },
+    {
+      id: "d",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "img/img-4.jpg",
+    },
+  ];
--- main.js
+++ main.js
@@ -1,105 +1,79 @@
-const shop = document.getElementById("shop");
+let shop = document.getElementById("shop");
 
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let basket = [];
-
-// !Data
-let shopItemsData = [
-    {
-        id:"1",
-        name: "Casual Shirts",
-        price: 45,
-        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "img/img-1.jpg"
-    },
-    {
-        id:"2",
-        name: "Office Shirts",
-        price: 100,
-        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "img/img-2.jpg"
-    },
-    {
-        id:"3",
-        name: "T Shirts",
-        price: 25,
-        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "img/img-3.jpg"
-    },
-    {
-        id:"4",
-        name: "Men Suits",
-        price: 300,
-        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        img: "img/img-4.jpg"
-    },
-    ];
-
-    let generateShop = () => { 
-        return (shop.innerHTML = shopItemsData.map((x) => {
-            let {id, name, price, desc, img } = x;
-            return `
-            <div id="product-${id}" class="shop-item">
-                <img src=${img} alt="">
-                <div class="details">
-                    <h2>${name}</h2>
-                    <p class="description">${desc}</p>
-                    <div class="price">
-                        <h2>$ ${price}</h2>
-                        <div class="quantity">
-                            <i onclick="decrement(${id})" class="bi bi-dash-lg minus"></i>
-                            <div id="${id}" class="quantityAmount">0</div>
-                            <i onclick="increment(${id})" class="bi bi-plus-lg plus"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `
-        }).join(""))
-    }
-
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      console.log(search)
+      return `
+      <div id="product-${id}" class="shop-item">
+      <img src=${img} alt="">
+      <div class="details">
+          <h2>${name}</h2>
+          <p class="description">${desc}</p>
+          <div class="price">
+              <h2>$ ${price}</h2>
+              <div class="quantity">
+                  <i onclick="decrement(${id})" class="bi bi-dash-lg minus"></i>
+                  <div id="${id}" class="quantityAmount">
+                  ${search.item === undefined ? 0 : search.item}
+                  </div>
+                  <i onclick="increment(${id})" class="bi bi-plus-lg plus"></i>
+              </div>
+          </div>
+      </div>
+  </div>
+    `;
+    })
+    .join(""));
+};
 
 generateShop();
 
-function increment(id) {
-    selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem)
-    
-    if(search === undefined) {
-        basket.push( {
-            id: selectedItem,
-            item: 1
-        });
-    }
-    else {
-        search.item += 1;
-    }       
-    update(selectedItem);
-}
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
 
-function decrement(id) {
-    selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem)
-    
-    if(search.item === 0) {
-        return
-    }
-    else {
-        search.item -= 1;
-    }
-    update(selectedItem);
-}
- 
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
 
-function update(id) {
-let search = basket.find((x) => x.id === id)
-let result  = document.getElementById(id)
-result.innerHTML = search.item; 
-calculation();
-}
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+  let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
 
 let calculation = () => {
-    let cartAmount = document.getElementById("cart-amount");
-    let total = basket.map((x) => x.item).reduce((x,y) => x+y, 0)
-    cartAmount.innerHTML = total;
-}
+  let cartIcon = document.getElementById("cart-amount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
